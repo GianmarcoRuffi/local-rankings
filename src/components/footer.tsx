73a05ac70b98aafd11bf280c165abd2aee12ab28
Gianmarco Ruffi <gianmarco.ruffi@outlook.it>
2026-03-03 17:21:40 +0100

    main: Fixed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,9 @@
 export function Footer() {
   return (
     <footer className="border-t bg-card py-3 text-center text-sm text-muted-foreground">
-      <div className="container mx-auto px-4">© Gianmarco Ruffi</div>
+      <div className="container mx-auto px-4">
+        © Made with ❤️ by Gianmarco Ruffi - "Massima dedizione alla causa! (cit.)"
+      </div>
     </footer>
   );
 }

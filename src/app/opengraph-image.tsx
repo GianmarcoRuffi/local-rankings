e1c1cc7ceec68875aa5837c91cd8c7a17fecbe91
Gianmarco Ruffi <gianmarco.ruffi@outlook.it>
2026-04-13 11:26:05 +0200

    develop: Updated Ranking Manager name

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from 'next/og';
 
 export const runtime = 'edge';
 
-export const alt = 'Rankings Manager Trophy';
+export const alt = 'Ranking Manager Trophy';
 export const size = {
   width: 1200,
   height: 630,
@@ -63,7 +63,7 @@ export default function Image() {
             display: 'flex',
           }}
         >
-          Rankings Manager
+          Ranking Manager
         </div>
         <div
           style={{
